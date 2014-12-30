7f30b8b7bbe08027d68c6425fd8de105124243ae
Jouni Roivas <jroivas@iki.fi>
2014-12-30 16:40:37 +0200

    Image provider

---
+++ images/images.py
@@ -0,0 +1,120 @@
+class ImageProvider(object):
+    def __init__(self):
+        pass
+
+    def list(self):
+        return []
+
+    def get(self, name):
+        pass
+
+    def clone(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+    def add(self, name, url):
+        pass
+
+
+class DummyImageProvider(ImageProvider):
+    def __init__(self, images={}):
+        self.images = images
+
+    def list(self):
+        """
+        >>> p = DummyImageProvider()
+        >>> p.list()
+        []
+        >>> p = DummyImageProvider({'test': '/path/to/test', 'dummy': '/path/to/dummy'})
+        >>> p.list()
+        ['test', 'dummy']
+        """
+        return self.images.keys()
+
+    def get(self, name):
+        """
+        >>> p = DummyImageProvider({'test': '/path/to/test', 'dummy': '/path/to/dummy'})
+        >>> p.get('') is None
+        True
+        >>> p.get('a') is None
+        True
+        >>> p.get('test') is None
+        False
+        >>> p.get('test')
+        '/path/to/test'
+        >>> p.get('dummy')
+        '/path/to/dummy'
+        """
+        if name in self.images:
+            return self.images[name]
+        return None
+
+    def clone(self, name):
+        """
+        >>> p = DummyImageProvider({'test': '/path/to/test', 'dummy': '/path/to/dummy'})
+        >>> p.clone('') is None
+        True
+        >>> p.clone(None) is None
+        True
+        >>> p.clone('test')
+        '/path/to/test_clone'
+        >>> p.clone('dummy')
+        '/path/to/dummy_clone'
+        """
+        img = self.get(name)
+        if img is not None:
+            return img + '_clone'
+
+        return None
+
+    def delete(self, name):
+        """
+        >>> p = DummyImageProvider({'test': '/path/to/test', 'dummy': '/path/to/dummy'})
+        >>> p.list()
+        ['test', 'dummy']
+        >>> p.delete('')
+        >>> p.list()
+        ['test', 'dummy']
+        >>> p.delete(None)
+        >>> p.list()
+        ['test', 'dummy']
+        >>> p.delete('test')
+        >>> p.list()
+        ['dummy']
+        >>> p.delete('dummy')
+        >>> p.list()
+        []
+        >>> p.delete('dummy')
+        >>> p.list()
+        []
+        """
+        res = {}
+        for item in self.images:
+            if item != name:
+                res[item] = self.images[item]
+        self.images = res
+
+    def add(self, name, url):
+        """
+        >>> p = DummyImageProvider({'test': '/path/to/test', 'dummy': '/path/to/dummy'})
+        >>> p.list()
+        ['test', 'dummy']
+        >>> p.add('test4', 'http://location')
+        >>> p.list()
+        ['test', 'dummy', 'test4']
+        >>> p.get('test4')
+        'http://location'
+        """
+        if name in self.images:
+            raise ValueError('Image already exists: %s' % (name))
+
+        self.images[name] = url
+
+def provider(sets):
+    image = sets.get('image', None)
+    if image is None or image == 'dummy':
+        return DummyImageProvider()
+
+    return None
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 def settings():
     return {
+        'image': 'dummy',
         'db': 'sqlite3',
         'db_table': 'klapi.db'
     }
